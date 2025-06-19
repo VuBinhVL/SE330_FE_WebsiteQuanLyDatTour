148c62e9f5131dca1a04020072a5d967be4d7887
Vu Binh <22520129@gm.uit.edu.vn>
2025-06-19 10:01:54 +0700

    Quản lý địa điểm: Hoàn thành gọi API lấy danh sách địa điểm và chức năng tìm kiếm theo tên địa điểm

--- src/app/pages/Admin/DestinationManagement/DestinationMainPage.js
+++ src/app/pages/Admin/DestinationManagement/DestinationMainPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./DestinationMainPage.css";
 import search from "../../../assets/icons/customer/header/search.png";
 import { MdOutlineAddBox } from "react-icons/md";
@@ -6,11 +6,33 @@ import { GoTrash } from "react-icons/go";
 import { MdOutlineRemoveRedEye } from "react-icons/md";
 import AddDestination from "../../../components/Admin/DestinationManagement/AddDestination/AddDestination";
 import DetailDestination from "../../../components/Admin/DestinationManagement/DetailDestination/DetailDestination";
+import { fetchGet } from "../../../lib/httpHandler";
+import { toast } from "react-toastify";
 
 export default function DestinationMainPage() {
   const [showAddDes, setShowAddDes] = useState(false);
   const [showDetailDes, setShowDetailDes] = useState(false);
+  const [destinationList, setDestinationList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
+  //Lấy danh sách địa điểm từ API
+  useEffect(() => {
+    const uri = "/api/admin/tourist-attraction";
+    fetchGet(
+      uri,
+      (data) => {
+        setDestinationList(data);
+        console.log("Danh sách địa điểm:", data);
+      },
+      (err) => toast.error(err.message),
+      () => toast.error("Lỗi kết nối đến máy chủ")
+    );
+  }, []);
+
+  // Hàm để lọc danh sách địa điểm theo tên
+  const filteredDestinations = destinationList.filter((destination) =>
+    destination.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
   const toggleAddDes = () => {
     setShowAddDes(!showAddDes);
   };
@@ -26,7 +48,9 @@ export default function DestinationMainPage() {
           <input
             type="text"
             className="input-search"
-            placeholder="Tìm kiếm ..."
+            placeholder="Tìm kiếm theo tên địa điểm..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
         </div>
         <button
@@ -51,22 +75,24 @@ export default function DestinationMainPage() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Vườn hoa Campuchiha</td>
-            <td>Xapmual</td>
-            <td>Cảnh quan du lich</td>
-            <td>
-              <MdOutlineRemoveRedEye
-                className="view-button"
-                title="Xem chi tiết"
-                onClick={toggleDetailDes}
-              />
-            </td>
-            <td>
-              <GoTrash className="delete-button" title="Xóa" />
-            </td>
-          </tr>
+          {filteredDestinations.map((item, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{item.name}</td>
+              <td>{item.location}</td>
+              <td>{item.categoryName}</td>
+              <td>
+                <MdOutlineRemoveRedEye
+                  className="view-button"
+                  title="Xem chi tiết"
+                  onClick={toggleDetailDes}
+                />
+              </td>
+              <td>
+                <GoTrash className="delete-button" title="Xóa" />
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
       {/* Hiển thị popup thêm địa điểm */}
